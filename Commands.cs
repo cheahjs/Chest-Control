5e10a14accd8e7578a9320627d5af8c44257466f
Deathmax <death@deathmax.tk>
2011-07-14 20:33:27 +0800

    Allow users to cancel selection state by executing the command again.

--- Commands.cs
+++ Commands.cs
@@ -16,14 +16,30 @@ namespace ChestControl
 
         private static void Set(CommandArgs args)
         {
-            ChestControl.Players[args.Player.Index].State = SettingState.Setting;
-            args.Player.SendMessage("Open a chest to protect it.", Microsoft.Xna.Framework.Color.BlueViolet);
+            if (ChestControl.Players[args.Player.Index].State == SettingState.Setting)
+            {
+                ChestControl.Players[args.Player.Index].State = SettingState.None;
+                args.Player.SendMessage("You are no longer selecting a chest.", Microsoft.Xna.Framework.Color.BlueViolet);
+            }
+            else
+            {
+                ChestControl.Players[args.Player.Index].State = SettingState.Setting;
+                args.Player.SendMessage("Open a chest to protect it.", Microsoft.Xna.Framework.Color.BlueViolet);
+            }
         }
 
         private static void UnSet(CommandArgs args)
         {
-            ChestControl.Players[args.Player.Index].State = SettingState.Deleting;
-            args.Player.SendMessage("Open a chest to delete it's protection.", Microsoft.Xna.Framework.Color.BlueViolet);
+            if (ChestControl.Players[args.Player.Index].State == SettingState.Deleting)
+            {
+                ChestControl.Players[args.Player.Index].State = SettingState.None;
+                args.Player.SendMessage("You are no longer selecting a chest.", Microsoft.Xna.Framework.Color.BlueViolet);
+            }
+            else
+            {
+                ChestControl.Players[args.Player.Index].State = SettingState.Deleting;
+                args.Player.SendMessage("Open a chest to delete it's protection.", Microsoft.Xna.Framework.Color.BlueViolet);
+            }
         }
     }
 }
