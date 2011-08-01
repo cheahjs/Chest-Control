fec1ced70e1f4a53a9a6b1160e032881946b0a03
Natrim <maly@kinet.cz>
2011-08-01 11:32:49 +0200

    chest info -None- owner string

--- ChestControl.cs
+++ ChestControl.cs
@@ -343,7 +343,7 @@ namespace ChestControl
 
                                 if (tplayer.Group.HasPermission("showchestinfo")) //if player should see chest info
                                 {
-                                    player.SendMessage("Chest Owner: " + chest.getOwner() + " || " + "Public: " + (chest.isLocked() ? "No" : "Yes") + " || " + "RegionShare: " + (chest.isRegionLocked() ? "Yes" : "No") + " || " + "Password: " + (chest.getPassword() == "" ? "No" : "Yes"), Color.Yellow);
+                                    player.SendMessage("Chest Owner: " + (chest.getOwner() == "" ? "-None-" : chest.getOwner()) + " || " + "Public: " + (chest.isLocked() ? "No" : "Yes") + " || " + "RegionShare: " + (chest.isRegionLocked() ? "Yes" : "No") + " || " + "Password: " + (chest.getPassword() == "" ? "No" : "Yes"), Color.Yellow);
                                 }
 
                                 if (!tplayer.Group.HasPermission("openallchests") && !chest.isOpenFor(player)) //if player doesnt has permission to see inside chest, then break and message
